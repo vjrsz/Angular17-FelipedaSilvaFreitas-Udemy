9f9d417b34e871cbc4eaeb3bbff03b5b8ab0876d
vjrsz <juniornunes1008@gmail.com>
2024-07-22 21:31:06 -0300

    feat Project Card

--- session04-project/src/components/components.module.ts
+++ session04-project/src/components/components.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import {ButtonComponent} from "./button/button.component";
 import {CardComponent} from "./card/card.component";
 
-
-
 @NgModule({
   declarations: [
     ButtonComponent,
